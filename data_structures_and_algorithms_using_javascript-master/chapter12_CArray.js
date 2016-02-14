60a02cf54d52956cb03d426ed8866d59e206d35d
SunshineLXH <463027965@qq.com>
2016-02-14 19:50:13 +0800

    add chapter12_CArray.js 3

--- data_structures_and_algorithms_using_javascript-master/chapter12_CArray.js
+++ data_structures_and_algorithms_using_javascript-master/chapter12_CArray.js
@@ -54,3 +54,15 @@ var numElements = 100;
 var myNums = new CArray(numElements);
 myNums.setData();
 print(myNums.toString());
+
+function bubbleSort() {
+    var numElements = this.dataStore.length;
+    var temp;
+    for (var outer = numElements; outer >= 2; --outer) {
+        for (var inner = 0; inner <= outer-1; ++inner) {
+            if (this.dataStore[inner] > this.dataStore[inner+1]) {
+                swap(this.dataStore, inner, inner+1);
+            }
+        }
+    }
+}
