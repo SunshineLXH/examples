9fd5b6d1db6ed18e39729b2f7d8906b57a62a88f
SunshineLXH <463027965@qq.com>
2016-02-25 22:37:06 +0800

    Update SpringFestivalCard.js

--- SpringFestivalCard/js/SpringFestivalCard.js
+++ SpringFestivalCard/js/SpringFestivalCard.js
@@ -1,10 +1,39 @@
-<<<<<<< HEAD
 /**
- * Created by SunshineLXH on 2016/2/18.
+ * Created by SunshineLXH on 2016/2/19.
  */
+//获取元素
 var music = document.getElementById("music");
-=======
-/**
- * Created by SunshineLXH on 2016/2/18.
- */
->>>>>>> origin/master
+var audio = document.getElementsByTagName('audio')[0];
+var page1 = document.getElementById('page1');
+var page2 = document.getElementById('page2');
+var page3 = document.getElementById('page3');
+
+//当音乐播放完停止时，自动停止光盘旋转效果
+audio.addEventListener('ended', function(event){
+    music.setAttribute('class', '');
+},false);
+
+//点击音乐图标，控制音乐播放效果
+music.addEventListener('touchstart',function(event){
+    if (audio.paused){
+        audio.play();
+        this.setAttribute('class','play');
+    }
+    else {
+        audio.pause();
+        this.setAttribute('class','');
+    }
+},false);
+
+//点击屏幕，开启好运2016
+page1.addEventListener('touchstart', function(event){
+    page1.style.display = 'none';
+    page2.style.display = 'block';
+    page3.style.display = 'block';
+    page3.style.top     = '100%';
+
+    setTimeout(function(){
+        page2.setAttribute('class', 'page fadeOut');
+        page3.setAttribute('class', 'page fadeIn');
+    }, 5500);
+}, false);
