6e6c519e602fdde8be7f4741de56caf6b63a3ae9
SunshineLXH <463027965@qq.com>
2016-04-08 23:09:57 +0800

    calendar 3

--- calendar/calendar.js
+++ calendar/calendar.js
@@ -1,3 +1,279 @@
 /**
  * Created by SunshineLXH on 2016/4/6.
  */
+var myDate;
+var getCurSeconds;
+var getCurMinutes;
+var getCurHours;
+var getCurYear;
+var getCurMonth;
+var getCurDate;
+var timeMonitor = document.getElementsByClassName('time-monitor')[0];
+var baseDatesBar = document.getElementsByClassName('base-dates-bar')[0];
+var baseDate = baseDatesBar.getElementsByTagName('span')[0];
+var baseWeekday = baseDatesBar.getElementsByTagName('span')[1];
+var dateShowPanel = document.getElementsByClassName('date-show-panel')[0];
+
+
+function addZero(time) {
+    return time = time < 10 ? '0' + time : time;
+}
+
+//获取当前时间
+function currentTime() {
+    myDate = new Date();
+
+    getCurSeconds = addZero(myDate.getSeconds());
+    getCurMinutes = addZero(myDate.getMinutes());
+    getCurHours = addZero(myDate.getHours());
+
+    timeMonitor.innerHTML = getCurHours + ':' + getCurMinutes + ':' + getCurSeconds;
+}
+
+function currentDate() {
+    myDate = new Date();
+
+    getCurYear = addZero(myDate.getFullYear());
+    getCurMonth = addZero(myDate.getMonth() + 1);
+    getCurDate = addZero(myDate.getDate());
+
+    switch(myDate.getDay()) {
+        case 1:
+            getCurWeekDay = '星期一';
+            break;
+        case 2:
+            getCurWeekDay = '星期二';
+            break;
+        case 3:
+            getCurWeekDay = '星期三';
+            break;
+        case 4:
+            getCurWeekDay = '星期四';
+            break;
+        case 5:
+            getCurWeekDay = '星期五';
+            break;
+        case 6:
+            getCurWeekDay = '星期六';
+            break;
+        default:
+            getCurWeekDay = '星期日';
+    }
+
+    baseDate.innerHTML = getCurYear + '-' + getCurMonth + '-' + getCurDate;
+    baseWeekday.innerHTML = getCurWeekDay;
+    dateShowPanel.innerHTML = myDate.getDate();
+
+}
+
+currentTime();
+currentDate();
+
+setInterval(currentTime, 1000);
+
+
+
+
+
+
+
+
+var lunarInfo=new Array(0x4bd8,0x4ae0,0xa570,0x54d5,0xd260,0xd950,0x5554,0x56af,0x9ad0,0x55d2,0x4ae0,0xa5b6,0xa4d0,0xd250,0xd255,0xb54f,0xd6a0,0xada2,0x95b0,0x4977,0x497f,0xa4b0,0xb4b5,0x6a50,0x6d40,0xab54,0x2b6f,0x9570,0x52f2,0x4970,0x6566,0xd4a0,0xea50,0x6a95,0x5adf,0x2b60,0x86e3,0x92ef,0xc8d7,0xc95f,0xd4a0,0xd8a6,0xb55f,0x56a0,0xa5b4,0x25df,0x92d0,0xd2b2,0xa950,0xb557,0x6ca0,0xb550,0x5355,0x4daf,0xa5b0,0x4573,0x52bf,0xa9a8,0xe950,0x6aa0,0xaea6,0xab50,0x4b60,0xaae4,0xa570,0x5260,0xf263,0xd950,0x5b57,0x56a0,0x96d0,0x4dd5,0x4ad0,0xa4d0,0xd4d4,0xd250,0xd558,0xb540,0xb6a0,0x95a6,0x95bf,0x49b0,0xa974,0xa4b0,0xb27a,0x6a50,0x6d40,0xaf46,0xab60,0x9570,0x4af5,0x4970,0x64b0,0x74a3,0xea50,0x6b58,0x5ac0,0xab60,0x96d5,0x92e0,0xc960,0xd954,0xd4a0,0xda50,0x7552,0x56a0,0xabb7,0x25d0,0x92d0,0xcab5,0xa950,0xb4a0,0xbaa4,0xad50,0x55d9,0x4ba0,0xa5b0,0x5176,0x52bf,0xa930,0x7954,0x6aa0,0xad50,0x5b52,0x4b60,0xa6e6,0xa4e0,0xd260,0xea65,0xd530,0x5aa0,0x76a3,0x96d0,0x4afb,0x4ad0,0xa4d0,0xd0b6,0xd25f,0xd520,0xdd45,0xb5a0,0x56d0,0x55b2,0x49b0,0xa577,0xa4b0,0xaa50,0xb255,0x6d2f,0xada0,0x4b63,0x937f,0x49f8,0x4970,0x64b0,0x68a6,0xea5f,0x6b20,0xa6c4,0xaaef,0x92e0,0xd2e3,0xc960,0xd557,0xd4a0,0xda50,0x5d55,0x56a0,0xa6d0,0x55d4,0x52d0,0xa9b8,0xa950,0xb4a0,0xb6a6,0xad50,0x55a0,0xaba4,0xa5b0,0x52b0,0xb273,0x6930,0x7337,0x6aa0,0xad50,0x4b55,0x4b6f,0xa570,0x54e4,0xd260,0xe968,0xd520,0xdaa0,0x6aa6,0x56df,0x4ae0,0xa9d4,0xa4d0,0xd150,0xf252,0xd520);
+
+var solarMonth=new Array(31,28,31,30,31,30,31,31,30,31,30,31);
+var Gan=new Array("甲","乙","丙","丁","戊","己","庚","辛","壬","癸");
+var Zhi=new Array("子","丑","寅","卯","辰","巳","午","未","申","酉","戌","亥");
+var Animals=new Array("鼠","牛","虎","兔","龙","蛇","马","羊","猴","鸡","狗","猪");
+var solarTerm = new Array("小寒","大寒","立春","雨水","惊蛰","春分","清明","谷雨","立夏","小满","芒种","夏至","小暑","大暑","立秋","处暑","白露","秋分","寒露","霜降","立冬","小雪","大雪","冬至");
+
+var sTermInfo = new Array(0,21208,42467,63836,85337,107014,128867,150921,173149,195551,218072,240693,263343,285989,308563,331033,353350,375494,397447,419210,440795,462224,483532,504758);
+var nStr1 = new Array('日','一','二','三','四','五','六','七','八','九','十');
+var nStr2 = new Array('初','十','廿','卅','卅');
+var monthName = new Array("一月","二月","三月","四月","五月","六月","七月","八月","九月","十月","十一月","十二月");
+
+//公历节日
+var sFtv = new Array(
+    "0101 元旦",
+    "0214 情人节",
+    "0308 妇女节",
+    "0312 植树节",
+    "0315 消费者权益日",
+    "0401 愚人节",
+    "0501 劳动节",
+    "0504 青年节",
+    "0512 护士节",
+    "0601 儿童节",
+    "0701 建党节",
+    "0801 建军节",
+    "0910 教师节",
+    "0928 孔子诞辰",
+    "1001 国庆节",
+    "1006 老人节",
+    "1024 联合国日",
+    "1224 平安夜",
+    "1225 圣诞节");
+//农历节日
+var lFtv = new Array(
+    "0101 春节",
+    "0115 元宵节",
+    "0505 端午节",
+    "0707 七夕情人节",
+    "0715 中元节",
+    "0815 中秋节",
+    "0909 重阳节",
+    "1208 腊八节",
+    "1224 小年");
+
+
+//返回农历y年的总天数
+function lYearDays(y) {
+    var i, sum = 348;
+    for (i = 0x8000; i > 0x8; i >>= 1) sum += (lunarInfo[y - 1900] & i) ? 1 : 0;
+    return (sum + leapDays(y));
+}
+//返回农历y年闰月的天数
+function leapDays(y) {
+    if (leapMonth(y)) return ((lunarInfo[y - 1900] & 0x10000) ? 30 : 29);
+    else return (0);
+}
+//判断y年的农历中那个月是闰月,不是闰月返回0
+function leapMonth(y) {
+    return (lunarInfo[y - 1900] & 0xf);
+}
+//返回农历y年m月的总天数
+function monthDays(y, m) {
+    return ((lunarInfo[y - 1900] & (0x10000 >> m)) ? 30 : 29);
+}
+//算出当前月第一天的农历日期和当前农历日期下一个月农历的第一天日期
+function Dianaday(objDate) {
+    var i, leap = 0, temp = 0;
+    var baseDate = new Date(1900, 0, 31);
+    var offset = (objDate - baseDate) / 86400000;
+    this.dayCyl = offset + 40;
+    this.monCyl = 14;
+    for (i = 1900; i < 2050 && offset > 0; i++) {
+        temp = lYearDays(i)
+        offset -= temp;
+        this.monCyl += 12;
+    }
+    if (offset < 0) {
+        offset += temp;
+        i--;
+        this.monCyl -= 12;
+    }
+    this.year = i;
+    this.yearCyl = i - 1864;
+    leap = leapMonth(i); //闰哪个月
+    this.isLeap = false;
+    for (i = 1; i < 13 && offset > 0; i++) {
+        if (leap > 0 && i == (leap + 1) && this.isLeap == false) {    //闰月
+            --i; this.isLeap = true; temp = leapDays(this.year);
+        }
+        else {
+            temp = monthDays(this.year, i);
+        }
+        if (this.isLeap == true && i == (leap + 1)) this.isLeap = false;    //解除闰月
+        offset -= temp;
+        if (this.isLeap == false) this.monCyl++;
+    }
+    if (offset == 0 && leap > 0 && i == leap + 1)
+        if (this.isLeap) { this.isLeap = false; }
+        else { this.isLeap = true; --i; --this.monCyl; }
+    if (offset < 0) { offset += temp; --i; --this.monCyl; }
+    this.month = i;
+    this.day = offset + 1;
+}
+//返回公历y年m+1月的天数
+function solarDays(y, m) {
+    if (m == 1)
+        return (((y % 4 == 0) && (y % 100 != 0) || (y % 400 == 0)) ? 29 : 28);
+    else
+        return (solarMonth[m]);
+}
+//记录公历和农历某天的日期
+function calElement(sYear, sMonth, sDay, week, lYear, lMonth, lDay, isLeap) {
+    this.isToday = false;
+    //公历
+    this.sYear = sYear;
+    this.sMonth = sMonth;
+    this.sDay = sDay;
+    this.week = week;
+    //农历
+    this.lYear = lYear;
+    this.lMonth = lMonth;
+    this.lDay = lDay;
+    this.isLeap = isLeap;
+    //节日记录
+    this.lunarFestival = ''; //农历节日
+    this.solarFestival = ''; //公历节日
+    this.solarTerms = ''; //节气
+}
+//返回某年的第n个节气为几日(从0小寒起算)
+function sTerm(y, n) {
+    var offDate = new Date((31556925974.7 * (y - 1900) + sTermInfo[n] * 60000) + Date.UTC(1900, 0, 6, 2, 5));
+    return (offDate.getUTCDate())
+}
+//保存y年m+1月的相关信息
+var fat = mat = 9;
+var eve = 0;
+function calendar(y, m) {
+    fat = mat = 0;
+    var sDObj, lDObj, lY, lM, lD = 1, lL, lX = 0, tmp1, tmp2;
+    var lDPOS = new Array(3);
+    var n = 0;
+    var firstLM = 0;
+    sDObj = new Date(y, m, 1);    //当月第一天的日期
+    this.length = solarDays(y, m);    //公历当月天数
+    this.firstWeek = sDObj.getDay();    //公历当月1日星期几
+    if ((m + 1) == 5) { fat = sDObj.getDay() }
+    if ((m + 1) == 6) { mat = sDObj.getDay() }
+    for (var i = 0; i < this.length; i++) {
+        if (lD > lX) {
+            sDObj = new Date(y, m, i + 1);    //当月第一天的日期
+            lDObj = new Dianaday(sDObj);     //农历
+            lY = lDObj.year;           //农历年
+            lM = lDObj.month;          //农历月
+            lD = lDObj.day;            //农历日
+            lL = lDObj.isLeap;         //农历是否闰月
+            lX = lL ? leapDays(lY) : monthDays(lY, lM); //农历当月最後一天
+            if (lM == 12) { eve = lX }
+            if (n == 0) firstLM = lM;
+            lDPOS[n++] = i - lD + 1;
+        }
+        this[i] = new calElement(y, m + 1, i + 1, nStr1[(i + this.firstWeek) % 7], lY, lM, lD++, lL);
+        if ((i + this.firstWeek) % 7 == 0) {
+            this[i].color = 'red';  //周日颜色
+        }
+    }
+    //节气
+    tmp1 = sTerm(y, m * 2) - 1;
+    tmp2 = sTerm(y, m * 2 + 1) - 1;
+    this[tmp1].solarTerms = solarTerm[m * 2];
+    this[tmp2].solarTerms = solarTerm[m * 2 + 1];
+    if ((this.firstWeek + 12) % 7 == 5)    //黑色星期五
+        this[12].solarFestival += '黑色星期五';
+    if (y == tY && m == tM) this[tD - 1].isToday = true;    //今日
+}
+//用中文显示农历的日期
+function cDay(d) {
+    var s;
+    switch (d) {
+        case 10:
+            s = '初十'; break;
+        case 20:
+            s = '二十'; break;
+            break;
+        case 30:
+            s = '三十'; break;
+            break;
+        default:
+            s = nStr2[Math.floor(d / 10)];
+            s += nStr1[d % 10];
+    }
+    return (s);
+}
+
+
+
+
+
