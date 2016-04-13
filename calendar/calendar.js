d6ae8d81500f4b128cc290d8b8fee5e4e46a1352
SunshineLXH <olixiaohui@163.com>
2016-04-14 01:05:46 +0800

    calendar/index.html

--- calendar/calendar.js
+++ calendar/calendar.js
@@ -1,138 +1,6 @@
 /**
  * Created by SunshineLXH on 2016/4/6.
  */
-//获取元素
-var myDate;
-var getCurSeconds;
-var getCurMinutes;
-var getCurHours;
-var getCurYear;
-var getCurMonth;
-var getCurDate;
-var offOn1 = false,
-    offOn2 = false,
-    offOn3 = false;
-var holidayPlan = ['2016节假日安排','元旦','春节','清明节','劳动节','端午节','中秋节','国庆节'];
-
-var timeMonitor = document.getElementsByClassName('time-monitor')[0];
-var baseDatesBar = document.getElementsByClassName('base-dates-bar')[0];
-var baseDate = baseDatesBar.getElementsByTagName('span')[0];
-var baseWeekday = baseDatesBar.getElementsByTagName('span')[1];
-var dateShowPanel = document.getElementsByClassName('date-show-panel')[0];
-var dateDetail = document.getElementsByClassName('dates-detail')[0];
-
-var yearControl = document.getElementsByClassName('year-control')[0];
-var control1 = yearControl.getElementsByClassName('control')[0];
-
-var monthControl = document.getElementsByClassName('month-control')[0];
-var control2 = monthControl.getElementsByClassName('control')[0];
-
-var holidayControl = document.getElementsByClassName('holiday-control')[0];
-var control3 = holidayControl.getElementsByClassName('control-holiday')[0];
-
-var prevYearBtn = document.getElementsByClassName('prev')[0];
-var nextYearBtn = document.getElementsByClassName('next')[0];
-
-var prevMonthBtn = document.getElementsByClassName('prev')[1];
-var nextMonthBtn = document.getElementsByClassName('next')[1];
-
-//返回今天
-var toToday = document.getElementsByClassName('btn-today')[0];
-
-dropList();
-
-
-function dropList () {
-    var listUl = document.createElement('ul');
-    var listUl2 = document.createElement('ul');
-    var listUl3 = document.createElement('ul');
-
-
-    for ( var i = 1900; i < 2101; i++ ) {
-        var yli = document.createElement('li');
-        yli.innerHTML = i + '年';
-        listUl.appendChild(yli);
-    }
-    listUl.className = "listUl1";
-    yearControl.appendChild(listUl);
-
-    for ( var j = 1; j < 13; j++ ) {
-        var mli = document.createElement('li');
-        mli.innerHTML = j + '月';
-        listUl2.appendChild(mli);
-    }
-    listUl2.className = "listUl2";
-    monthControl.appendChild(listUl2);
-
-    for (var k = 0; k < holidayPlan.length; k++ ) {
-        var hli = document.createElement('li');
-        hli.innerHTML = holidayPlan[k];
-        listUl3.appendChild(hli);
-    }
-    listUl3.className = "listUl3";
-    holidayControl.appendChild(listUl3);
-
-
-
-}
-
-var listUl1 = document.getElementsByClassName('listUl1')[0];
-var listUl2 = document.getElementsByClassName('listUl2')[0];
-var listUl3 = document.getElementsByClassName('listUl3')[0];
-
-
-
-function showHidden1() {
-    if ( offOn1 ) {
-        listUl1.style.display = 'block';
-        offOn1 = false;
-    }
-    else {
-        listUl1.style.display = 'none';
-        offOn1 = true;
-    }
-}
-
-function showHidden2() {
-    if ( offOn2 ) {
-        listUl2.style.display = 'block';
-        offOn2 = false;
-    }
-    else {
-        listUl2.style.display = 'none';
-        offOn2 = true;
-    }
-}
-
-function showHidden3() {
-    if ( offOn3 ) {
-        listUl3.style.display = 'block';
-        offOn3 = false;
-    }
-    else {
-        listUl3.style.display = 'none';
-        offOn3 = true;
-    }
-}
-
-var yearBtn = control1.getElementsByClassName('year')[0];
-var trigger1 = control1.getElementsByClassName('trigger')[0];
-
-var monthBtn = control2.getElementsByClassName('month')[0];
-var trigger2 = control2.getElementsByClassName('trigger')[0];
-
-var holidayBtn = control3.getElementsByClassName('holiday')[0];
-var trigger3 = control3.getElementsByClassName('trigger')[0];
-
-
-addEvent( yearBtn, 'click', showHidden1 );
-addEvent( trigger1, 'click', showHidden1 );
-
-addEvent( monthBtn, 'click', showHidden2 );
-addEvent( trigger2, 'click', showHidden2 );
-
-addEvent( holidayBtn, 'click', showHidden3 );
-addEvent( trigger3, 'click', showHidden3 );
 
 //绑定事件函数
 function addEvent( obj, type, handle ) {
@@ -150,10 +18,163 @@ function addEvent( obj, type, handle ) {
     return false;
 }
 
+//给小于10的数字前加0
+function addZero(time) {
+    return time = time < 10 ? '0' + time : time;
+}
+
+//声明元素
+//当前秒、分、时、年、月、日的变量
+var myDate,
+    getCurSeconds,
+    getCurMinutes,
+    getCurHours,
+    getCurYear,
+    getCurMonth,
+    getCurDate;
+
+//日历上方三个按钮初始化状态
+var offOn1 = true,
+    offOn2 = true,
+    offOn3 = true;
+
+//节假日安排中节假日信息数组
+var holidayPlan = ['2016节假日安排','元旦','春节','清明节','劳动节','端午节','中秋节','国庆节'];
+
+//获取元素
+var timeMonitor = document.getElementsByClassName('time-monitor')[0];
+var baseDatesBar = document.getElementsByClassName('base-dates-bar')[0];
+var baseDate = baseDatesBar.getElementsByTagName('span')[0];
+var baseWeekday = baseDatesBar.getElementsByTagName('span')[1];
+var dateShowPanel = document.getElementsByClassName('date-show-panel')[0];
+var dateDetail = document.getElementsByClassName('dates-detail')[0];
+
+var yearControl = document.getElementsByClassName('year-control')[0];
+var control1 = yearControl.getElementsByClassName('control')[0];
+
+var monthControl = document.getElementsByClassName('month-control')[0];
+var control2 = monthControl.getElementsByClassName('control')[0];
+
+var holidayControl = document.getElementsByClassName('holiday-control')[0];
+var control3 = holidayControl.getElementsByClassName('control-holiday')[0];
+
+var yearBtn = control1.getElementsByClassName('year')[0];
+var trigger1 = control1.getElementsByClassName('trigger')[0];
+
+var monthBtn = control2.getElementsByClassName('month')[0];
+var trigger2 = control2.getElementsByClassName('trigger')[0];
+
+var holidayBtn = control3.getElementsByClassName('holiday')[0];
+var trigger3 = control3.getElementsByClassName('trigger')[0];
+
+//上一年、下一年按钮， 上一月、下一月按钮
+var prevYearBtn = document.getElementsByClassName('prev')[0];
+var nextYearBtn = document.getElementsByClassName('next')[0];
+
+var prevMonthBtn = document.getElementsByClassName('prev')[1];
+var nextMonthBtn = document.getElementsByClassName('next')[1];
+
+//返回今天
+var toToday = document.getElementsByClassName('btn-today')[0];
+
+dropList();
 
 
-/*下面的左边部分*/
+
+
+
+//封装下拉框函数
+function dropList () {
+    var listUl = document.createElement('ul');
+    var listUl2 = document.createElement('ul');
+    var listUl3 = document.createElement('ul');
+
+    //生成年下拉框
+    for ( var i = 1900; i < 2101; i++ ) {
+        var yli = document.createElement('li');
+        yli.innerHTML = i + '年';
+        listUl.appendChild(yli);
+    }
+    listUl.className = "listUl1";
+    yearControl.appendChild(listUl);
+
+    //生成月下拉框
+    for ( var j = 1; j < 13; j++ ) {
+        var mli = document.createElement('li');
+        mli.innerHTML = j + '月';
+        listUl2.appendChild(mli);
+    }
+    listUl2.className = "listUl2";
+    monthControl.appendChild(listUl2);
+
+    //生成节假日下拉框
+    for (var k = 0; k < holidayPlan.length; k++ ) {
+        var hli = document.createElement('li');
+        hli.innerHTML = holidayPlan[k];
+        listUl3.appendChild(hli);
+    }
+    listUl3.className = "listUl3";
+    holidayControl.appendChild(listUl3);
+}
+
+//三个按钮
+var listUl1 = document.getElementsByClassName('listUl1')[0],
+    listUl2 = document.getElementsByClassName('listUl2')[0],
+    listUl3 = document.getElementsByClassName('listUl3')[0];
+
+//年按钮显示与隐藏
+function showHidden1() {
+    if ( offOn1 ) {
+        listUl1.style.display = 'block';
+        offOn1 = false;
+    }
+    else {
+        listUl1.style.display = 'none';
+        offOn1 = true;
+    }
+}
+
+//月按钮显示与隐藏
+function showHidden2() {
+    if ( offOn2 ) {
+        listUl2.style.display = 'block';
+        offOn2 = false;
+    }
+    else {
+        listUl2.style.display = 'none';
+        offOn2 = true;
+    }
+}
+
+//节假日显示与隐藏
+function showHidden3() {
+    if ( offOn3 ) {
+        listUl3.style.display = 'block';
+        offOn3 = false;
+    }
+    else {
+        listUl3.style.display = 'none';
+        offOn3 = true;
+    }
+}
+
+//分别给年按钮、月按钮、节假日按钮绑定事件
+addEvent( yearBtn, 'click', showHidden1 );
+addEvent( trigger1, 'click', showHidden1 );
+
+addEvent( monthBtn, 'click', showHidden2 );
+addEvent( trigger2, 'click', showHidden2 );
+
+addEvent( holidayBtn, 'click', showHidden3 );
+addEvent( trigger3, 'click', showHidden3 );
+
+addEvent( toToday, 'click', initial );
+
+
+
+/*日历下面部分的左边部分*/
 //首先根据获取的当前日期，计算当月有多少天，当月第一天是周几
+
 //循环生成42个方块
 for ( var i = 0; i < 42; i++ ) {
     var oLi = document.createElement('li');
@@ -179,80 +200,127 @@ for ( var i = 0; i < 42; i++ ) {
     dateDetail.appendChild(oLi);
 }
 
+//获取42个li元素
 var aLi = dateDetail.getElementsByTagName('li');
 var solarDay = dateDetail.getElementsByClassName('solarDay');
 
-for ( var i = 0; i < solarDay.length; i++ ) {
-
+//将周六和周日字体颜色为红色
+for ( var i = 5; i < 6; i++ ) {
+    for ( var j = 0; j < 6; j++ ) {
+        aLi[i + 7 * j].style.color = '#B04343';
+        aLi[(i + 1) + 7 * j ].style.color = '#B04343';
+    }
 }
 
 showDate();
 
+//
 function showDate(year, month, day) {
     var selectDate = new Date();
     year = year ? year : selectDate.getFullYear();
     month = month ? month : (selectDate.getMonth() + 1);
     day = day ? day : selectDate.getDate();
 
-    var dayNum = 0;
+    var dayNum = 0,
+        preMonthNum = 0;
 
     dayNum = getMonthNum(month);
+
+    preMonthNum = getMonthNum(month - 1);
 
     selectDate.setFullYear(year);
     selectDate.setMonth(month - 1);
     selectDate.setDate(1);
 
+
+
     switch ( selectDate.getDay() ) {
         case 0:
-            for ( var i = 0; i < dayNum; i++ ) {
-                solarDay[i + 6].innerHTML = i + 1;
-            }
+            ab(5, day, dayNum, preMonthNum);
             break;
         case 1:
-            for ( var i = 0; i < dayNum; i++ ) {
-                solarDay[i].innerHTML = i + 1;
-            }
+            ab(5, day, dayNum, preMonthNum);
             break;
         case 2:
-            for ( var i = 0; i < dayNum; i++ ) {
-                solarDay[i + 1].innerHTML = i + 1;
-            }
+            ab(1, day, dayNum, preMonthNum);
             break;
         case 3:
-            for ( var i = 0; i < dayNum; i++ ) {
-                solarDay[i + 2].innerHTML = i + 1;
-            }
+            ab(2, day, dayNum, preMonthNum);
             break;
         case 4:
-            for ( var i = 0; i < dayNum; i++ ) {
-                solarDay[i + 3].innerHTML = i + 1;
-            }
+            ab(3, day, dayNum, preMonthNum);
             break;
         case 5:
-            for ( var i = 0; i < dayNum; i++ ) {
-                solarDay[i + 4].innerHTML = i + 1;
-            }
+            ab(4, day, dayNum, preMonthNum);
             break;
         case 6:
-            for ( var i = 0; i < dayNum; i++ ) {
-                solarDay[i + 5].innerHTML = i + 1;
-            }
+            ab(5, day, dayNum, preMonthNum);
             break;
-
     }
+}
 
-    var preMonthNum = getMonthNum(month - 1);
-
-    for ( var m = 3; m >= 0; m-- ) {
-        for ( var j = preMonthNum.length; j > 0; j--) {
-            console.log(1)
-            solarDay[m].innerHTML = j;
+function ab(num, day, dayNum, preMonthNum) {
+    //当月
+    for ( var i = 0; i < dayNum; i++ ) {
+        solarDay[i + num].innerHTML = i + 1;
+        if ( i == day ) {
+            var div = document.createElement('div');
+            div.className = 'td';
+            solarDay[i + num - 1].parentNode.appendChild(div);
         }
     }
+    //前一月
+    for ( var i = num + dayNum, j = 1; i < 42, j <= 42 - num - dayNum; i++, j++ ) {
+        solarDay[i].innerHTML = j;
+        solarDay[i].parentNode.style.opacity = 0.5;
+        solarDay[i].parentNode.style.background = '#F7F5F3';
+    }
+    //后一月
+    for ( var p = 0, q = preMonthNum - num + 1; p < num, q <= preMonthNum; p++, q++ ) {
+        solarDay[p].innerHTML = q;
+        solarDay[p].parentNode.style.opacity = 0.5;
+        solarDay[p].parentNode.style.background = '#F7F5F3';
+    }
+}
 
+initial();
 
-    var nextMonthNum = getMonthNum(month + 1);
+//年选择按钮、月选择按钮初始化
+function initial() {
+    myDate = new Date();
+    yearBtn.innerHTML = myDate.getFullYear() + '年';
+    monthBtn.innerHTML = myDate.getMonth() + '月';
 
+    getCurYear = myDate.getFullYear();
+    getCurMonth = addZero(myDate.getMonth() + 1);
+    getCurDate = addZero(myDate.getDate());
+
+    switch(myDate.getDay()) {
+        case 1:
+            getCurWeekDay = '星期一';
+            break;
+        case 2:
+            getCurWeekDay = '星期二';
+            break;
+        case 3:
+            getCurWeekDay = '星期三';
+            break;
+        case 4:
+            getCurWeekDay = '星期四';
+            break;
+        case 5:
+            getCurWeekDay = '星期五';
+            break;
+        case 6:
+            getCurWeekDay = '星期六';
+            break;
+        default:
+            getCurWeekDay = '星期日';
+    }
+
+    baseDate.innerHTML = getCurYear + '-' + getCurMonth + '-' + getCurDate;
+    baseWeekday.innerHTML = getCurWeekDay;
+    dateShowPanel.innerHTML = myDate.getDate();
 
 
 }
@@ -295,10 +363,7 @@ function isLeapYear( year ) {
 
 
 /*下面的右边部分*/
-//给小于10的数字前加0
-function addZero(time) {
-    return time = time < 10 ? '0' + time : time;
-}
+
 
 //获取当前时间：时分秒
 function currentTime() {
@@ -311,51 +376,13 @@ function currentTime() {
     timeMonitor.innerHTML = getCurHours + ':' + getCurMinutes + ':' + getCurSeconds;
 }
 
-//获取当前日期：年月日，星期几
-function currentDate() {
-    myDate = new Date();
-
-    getCurYear = addZero(myDate.getFullYear());
-    getCurMonth = addZero(myDate.getMonth() + 1);
-    getCurDate = addZero(myDate.getDate());
-
-    switch(myDate.getDay()) {
-        case 1:
-            getCurWeekDay = '星期一';
-            break;
-        case 2:
-            getCurWeekDay = '星期二';
-            break;
-        case 3:
-            getCurWeekDay = '星期三';
-            break;
-        case 4:
-            getCurWeekDay = '星期四';
-            break;
-        case 5:
-            getCurWeekDay = '星期五';
-            break;
-        case 6:
-            getCurWeekDay = '星期六';
-            break;
-        default:
-            getCurWeekDay = '星期日';
-    }
-
-    baseDate.innerHTML = getCurYear + '-' + getCurMonth + '-' + getCurDate;
-    baseWeekday.innerHTML = getCurWeekDay;
-    dateShowPanel.innerHTML = myDate.getDate();
-
-}
 
 currentTime();
-currentDate();
 
 //给当前时间函数开个定时器，让时分秒走起来
 setInterval(currentTime, 1000);
 
-//农历1900-2100的润大小信息表
-
+//农历1900-2100的润信息表
 var lunarInfo=[0x4bd8,0x4ae0,0xa570,0x54d5,0xd260,0xd950,0x5554,0x56af,0x9ad0,0x55d2,0x4ae0,0xa5b6,0xa4d0,0xd250,0xd255,0xb54f,0xd6a0,0xada2,0x95b0,0x4977,0x497f,0xa4b0,0xb4b5,0x6a50,0x6d40,0xab54,0x2b6f,0x9570,0x52f2,0x4970,0x6566,0xd4a0,0xea50,0x6a95,0x5adf,0x2b60,0x86e3,0x92ef,0xc8d7,0xc95f,0xd4a0,0xd8a6,0xb55f,0x56a0,0xa5b4,0x25df,0x92d0,0xd2b2,0xa950,0xb557,0x6ca0,0xb550,0x5355,0x4daf,0xa5b0,0x4573,0x52bf,0xa9a8,0xe950,0x6aa0,0xaea6,0xab50,0x4b60,0xaae4,0xa570,0x5260,0xf263,0xd950,0x5b57,0x56a0,0x96d0,0x4dd5,0x4ad0,0xa4d0,0xd4d4,0xd250,0xd558,0xb540,0xb6a0,0x95a6,0x95bf,0x49b0,0xa974,0xa4b0,0xb27a,0x6a50,0x6d40,0xaf46,0xab60,0x9570,0x4af5,0x4970,0x64b0,0x74a3,0xea50,0x6b58,0x5ac0,0xab60,0x96d5,0x92e0,0xc960,0xd954,0xd4a0,0xda50,0x7552,0x56a0,0xabb7,0x25d0,0x92d0,0xcab5,0xa950,0xb4a0,0xbaa4,0xad50,0x55d9,0x4ba0,0xa5b0,0x5176,0x52bf,0xa930,0x7954,0x6aa0,0xad50,0x5b52,0x4b60,0xa6e6,0xa4e0,0xd260,0xea65,0xd530,0x5aa0,0x76a3,0x96d0,0x4afb,0x4ad0,0xa4d0,0xd0b6,0xd25f,0xd520,0xdd45,0xb5a0,0x56d0,0x55b2,0x49b0,0xa577,0xa4b0,0xaa50,0xb255,0x6d2f,0xada0,0x4b63,0x937f,0x49f8,0x4970,0x64b0,0x68a6,0xea5f,0x6b20,0xa6c4,0xaaef,0x92e0,0xd2e3,0xc960,0xd557,0xd4a0,0xda50,0x5d55,0x56a0,0xa6d0,0x55d4,0x52d0,0xa9b8,0xa950,0xb4a0,0xb6a6,0xad50,0x55a0,0xaba4,0xa5b0,0x52b0,0xb273,0x6930,0x7337,0x6aa0,0xad50,0x4b55,0x4b6f,0xa570,0x54e4,0xd260,0xe968,0xd520,0xdaa0,0x6aa6,0x56df,0x4ae0,0xa9d4,0xa4d0,0xd150,0xf252,0xd520];
 
 var solarMonth=[31,28,31,30,31,30,31,31,30,31,30,31];
@@ -371,7 +398,6 @@ var Animals=["鼠","牛","虎","兔","龙","蛇","马","羊","猴","鸡","狗","
 
 //24节气
 var solarTerm = ["小寒","大寒","立春","雨水","惊蛰","春分","清明","谷雨","立夏","小满","芒种","夏至","小暑","大暑","立秋","处暑","白露","秋分","寒露","霜降","立冬","小雪","大雪","冬至"];
-
 
 var sTermInfo = [0,21208,42467,63836,85337,107014,128867,150921,173149,195551,218072,240693,263343,285989,308563,331033,353350,375494,397447,419210,440795,462224,483532,504758];
 
@@ -471,7 +497,9 @@ function Dianaday(objDate) {
     this.isLeap = false;
     for (i = 1; i < 13 && offset > 0; i++) {
         if (leap > 0 && i == (leap + 1) && this.isLeap == false) {    //闰月
-            --i; this.isLeap = true; temp = leapDays(this.year);
+            --i;
+            this.isLeap = true;
+            temp = leapDays(this.year);
         }
         else {
             temp = monthDays(this.year, i);
@@ -481,9 +509,18 @@ function Dianaday(objDate) {
         if (this.isLeap == false) this.monCyl++;
     }
     if (offset == 0 && leap > 0 && i == leap + 1)
-        if (this.isLeap) { this.isLeap = false; }
-        else { this.isLeap = true; --i; --this.monCyl; }
-    if (offset < 0) { offset += temp; --i; --this.monCyl; }
+        if (this.isLeap) {
+            this.isLeap = false;
+        }
+        else {
+            this.isLeap = true;
+            --i;
+            --this.monCyl;
+        }
+    if (offset < 0) {
+        offset += temp;
+        --i; --this.monCyl;
+    }
     this.month = i;
     this.day = offset + 1;
 }
@@ -697,6 +734,8 @@ function solar2lunar(year, month, day) {
 
     return {'lYear':year,'lMonth':month,'lDay':day,'Animal':calendar.getAnimal(year),'IMonthCn':(isLeap?"\u95f0":'')+calendar.toChinaMonth(month),'IDayCn':calendar.toChinaDay(day),'cYear':y,'cMonth':m,'cDay':d,'gzYear':gzY,'gzMonth':gzM,'gzDay':gzD,'isToday':isToday,'isLeap':isLeap,'nWeek':nWeek,'ncWeek':"\u661f\u671f"+cWeek,'isTerm':isTerm,'Term':Term};
 }
+
+
 function lunar2solar(y,m,d,isLeapMonth) {	//参数区间1900.1.31~2100.12.1
     var leapOffset = 0;
     var leapMonth = calendar.leapMonth(y);
@@ -733,7 +772,24 @@ function lunar2solar(y,m,d,isLeapMonth) {	//参数区间1900.1.31~2100.12.1
     return calendar.solar2lunar(cY,cM,cD);
 }
 
+//
+function calDay ( solarYear, solarMonth, solarDay, week, lunarYear, lunarMonth, lunarDay, isLeapYear) {
+    this.isToday = false;
 
+    this.solarYear = solarYear;
+    this.solarMonth = solarMonth;
+    this.solarDay = solarDay;
+    this.week = week;
+
+    this.lunarYear = lunarYear;
+    this.lunarMonth = lunarMonth;
+    this.lunarDay = lunarDay;
+    this.isLeapYear = isLeapYear;
+
+    this.lunarFestival = '';
+    this.solarFestival = '';
+    this.solarTerms = '';
+}
 
 
 
@@ -851,6 +907,9 @@ function cDay(d) {
     }
     return (s);
 }
+
+
+
 
 
 
